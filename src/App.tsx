81bab82e290e9720d3c7ae8a206447ba8e1ddab4
sy <sy.smythe@gmail.com>
2023-05-03 07:51:24 -0400

    Scaffold basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from 'react-bootstrap'
+import { Route, Routes, Navigate } from "react-router-dom"
 
 const App = () => {
-  return <h1>Notes Bootstrap</h1>
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>} />
+        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="*" element={<Navigate to="/"/>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+      </Routes>
+    </Container>
+  );
 }
 
 export default App
